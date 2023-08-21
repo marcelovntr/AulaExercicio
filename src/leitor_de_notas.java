5ea8202899821a65050cc21b827440e791a7f398
mvntr <marcelovieirantr@gmail.com>
2023-08-21 14:54:28 -0300

    agora tudo junto e misturado

--- src/leitor_de_notas.java
+++ src/leitor_de_notas.java
@@ -12,17 +12,43 @@ public class leitor_de_notas {
 		
 	List<Double> notas=new ArrayList<>(); 
 	int soma=0;	
-		
+	int cc=0;
+	double menor=0;
+	double maior=0;
+	
 	for(int i=0;i<5;i++) {
 		System.out.println("digite a nota de 5 alunos:");
 		notas.add(input.nextDouble());
 		soma+=notas.get(i);
+		double nota=notas.get(i);
+		
+		if(nota<6) {
+			cc++;
+		}
+	
+		if(i==0) {
+			maior=nota;
+			menor=nota;
+		}
+		
+		else if(nota>maior) {
+			maior=nota;
+		}
+	
+		if(nota<menor) {
+			menor=nota;
+		}
+	
 	
 	}
 	
+	double media=soma/5;
 	System.out.println("total: "+notas.size());
-	System.out.println(soma);
-	
+	System.out.println("soma: "+soma);
+	System.out.println("média: "+media);
+	System.out.println("notas abaixo de 6: "+cc);
+	System.out.println("nota maior: "+maior);
+	System.out.println("nota maior: "+menor);
 	
 	
 	
